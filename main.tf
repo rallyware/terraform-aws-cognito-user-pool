7360ec878007b57770dad4a8b2d1b6659cf0bf39
SweetOps <vladimir.fess@gmail.com>
2022-08-11 10:06:46 +0200

    upd

--- main.tf
+++ main.tf
@@ -6,14 +6,29 @@ locals {
 resource "aws_cognito_user_pool" "default" {
   count = local.enabled ? 1 : 0
 
-  name = module.this.id
-  tags = module.this.tags
+  name                     = module.this.id
+  tags                     = module.this.tags
+  auto_verified_attributes = var.auto_verified_attributes
+  username_attributes      = var.username_attributes
+
+  admin_create_user_config {
+    allow_admin_create_user_only = var.allow_admin_create_user_only
+  }
+
+  password_policy {
+    minimum_length                   = var.password_policy["minimum_length"]
+    require_lowercase                = var.password_policy["require_lowercase"]
+    require_numbers                  = var.password_policy["require_numbers"]
+    require_symbols                  = var.password_policy["require_symbols"]
+    require_uppercase                = var.password_policy["require_uppercase"]
+    temporary_password_validity_days = var.password_policy["temporary_password_validity_days"]
+  }
 }
 
 resource "aws_cognito_user_pool_domain" "default" {
-  count = local.enabled && var.domain != null ? 1 : 0
+  count = local.enabled ? 1 : 0
 
-  domain          = var.domain
+  domain          = coalesce(var.domain, module.this.id)
   certificate_arn = var.certificate_arn
   user_pool_id    = local.user_pool_id
 }
--- variables.tf
+++ variables.tf
@@ -1,3 +1,44 @@
+variable "auto_verified_attributes" {
+  type        = list(string)
+  default     = ["email"]
+  description = "Attributes to be auto-verified. Valid values: email, phone_number."
+}
+
+variable "username_attributes" {
+  type        = list(string)
+  default     = ["email"]
+  description = "Whether email addresses or phone numbers can be specified as usernames when a user signs up. Conflicts with alias_attributes."
+}
+
+variable "allow_admin_create_user_only" {
+  type        = bool
+  default     = true
+  description = "Set to False if users can sign themselves up via an app."
+}
+
+variable "password_policy" {
+  type = object(
+    {
+      minimum_length                   = number
+      require_lowercase                = bool
+      require_numbers                  = bool
+      require_symbols                  = bool
+      require_uppercase                = bool
+      temporary_password_validity_days = number
+    }
+  )
+  default = {
+    minimum_length                   = 8
+    require_lowercase                = true
+    require_numbers                  = true
+    require_symbols                  = true
+    require_uppercase                = true
+    temporary_password_validity_days = 3
+  }
+
+  description = "Configuration blocked for information about the user pool password policy."
+}
+
 variable "domain" {
   type        = string
   default     = null
